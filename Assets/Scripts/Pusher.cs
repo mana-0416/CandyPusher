862035d96d316df37810dc858f8fb6341ce47b4a
mana-0416 <sia.ekaki28@gmail.com>
2026-05-07 12:16:37 +0900

    name

---
+++ Assets/Scripts/Pusher.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Pusher : MonoBehaviour
+{
+    public float spead = 1f;
+    public float muvePower = 5f;
+    private Vector3 startPosition;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startPosition = this.transform.position;
+        Debug.Log("ÉQÅ[ÉÄÇ™äJénÇµÇΩÇÊ");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
