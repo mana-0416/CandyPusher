a83462c4e8a7172980605591d0b512bb09d5e069
mana-0416 <sia.ekaki28@gmail.com>
2026-06-18 11:47:01 +0900

    Reproduction of UI
    落ちたキャンディをカウントしてスコアを数えるようにした。
    UIの再現を行った。

--- Assets/Scripts/StageOut.cs
+++ Assets/Scripts/StageOut.cs
@@ -1,10 +1,16 @@
 using UnityEngine;
-
+using UnityEngine.UI;
 public class StageOut : MonoBehaviour
 {
-   
+    //Score = 0,1,2....
+    private int Score;
+    public Text scoreText;
     void OnTriggerEnter(Collider other)
     {
+        //Sccore = Score + 1;
+        Score += 1;
+        Debug.Log($"{Score}");
+        scoreText.text = $"Score: {Score}";
         Debug.Log($"{other.name}‚ª‚·‚è”²‚¯‚Ü‚µ‚½");
         Destroy(other.gameObject);
     }
